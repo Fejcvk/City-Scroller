9fdf473163d8c03b43d28b8bf7ad67d14d824844
Fejcvk <>
2019-03-23 11:46:36 +0100

    hit test for detecting moon implemented

--- ViewController.swift
+++ ViewController.swift
@@ -11,68 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     var scrollView: InfiniteScrollView { return view as! InfiniteScrollView}
-    var grabOffsetVector:CGVector = CGVector.zero
-    var moonView: UIView?
-    var originalOffset: CGFloat = 0
     
     override func loadView() {
         self.view = InfiniteScrollView.init()
-        self.moonView = createMoon()
-        view.addSubview(moonView!)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
-
-    }
-
-    private func createMoon() -> UIView {
-        let side = 100
-        let size = CGSize(width: side, height: side)
-        let circle = UIView(frame: CGRect(origin: .zero, size: size))
-        circle.backgroundColor = .white
-        circle.layer.cornerRadius = CGFloat(side) * 0.5
-        
-        let moveGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
-        circle.addGestureRecognizer(moveGesture)
-        
-        return circle
     }
     
     private func positionMoon() {
-        guard let moonView = moonView else { return }
-        let newPosition = originalOffset > 0 ? scrollView.contentOffset.x + (scrollView.frame.size.width - originalOffset) : scrollView.contentOffset.x
+        guard let moonView = scrollView.moonView else { return }
+        let newPosition = scrollView.originalOffset > 0 ? scrollView.contentOffset.x + (scrollView.frame.size.width - scrollView.originalOffset) : scrollView.contentOffset.x
         moonView.frame.origin.x = newPosition
-    }
-    
-    @objc func handleLongPress(_ gesture: UILongPressGestureRecognizer){
-        guard let movedCircle = gesture.view else { return }
-        
-        switch gesture.state {
-        case .began:
-            let grabPoint = gesture.location(in: view)
-            let circleCenter = movedCircle.center
-            let grabOffset = CGVector(dx: grabPoint.x - circleCenter.x, dy: grabPoint.y - circleCenter.y)
-            self.grabOffsetVector = grabOffset
-            self.view.sendSubviewToBack(movedCircle)
-            UIView.animate(withDuration: 0.2) {
-                movedCircle.alpha = 0.5
-                movedCircle.transform = .init(scaleX: 1.2, y: 1.2)
-            }
-        case .changed:
-            let grabOffset = self.grabOffsetVector
-            let touchLocation = gesture.location(in: self.view)
-            movedCircle.center = CGPoint(x: touchLocation.x - grabOffset.dx, y: touchLocation.y - grabOffset.dy)
-            originalOffset = abs(movedCircle.frame.origin.x - scrollView.bounds.maxX)
-        case .ended, .cancelled:
-            UIView.animate(withDuration: 0.2) {
-                movedCircle.alpha = 1
-                movedCircle.transform = .identity
-            }
-        default:
-            break
-        }
     }
 }
 
--- Views/InfiniteScrollView.swift
+++ Views/InfiniteScrollView.swift
@@ -12,10 +12,19 @@ class InfiniteScrollView: UIScrollView {
     let buildingContainerView = UIView()
     private var visibleBuildings: [BuildingView] = []
     
+    var grabOffsetVector:CGVector = CGVector.zero
+    var moonView: UIView?
+    var originalOffset: CGFloat = 0
+    
+    
     init() {
-        super.init(frame: .zero)
+        super.init(frame:.zero)
         self.addSubview(buildingContainerView)
         isPagingEnabled = true
+        
+        self.moonView = createMoon()
+        self.addSubview(self.moonView!)
+        self.sendSubviewToBack(self.moonView!)
     }
     
     override var frame: CGRect {
@@ -103,14 +112,63 @@ class InfiniteScrollView: UIScrollView {
         }
     }
     
-    
-    
     func setup(size: CGSize) {
         
-        contentSize = CGSize(width: size.width * 3, height: size.height)
+        contentSize = CGSize(width: size.width * 3, height: BuildingView.maxBuildingHeight)
         //royal blue
         backgroundColor = UIColor(red:65.0/255, green:105.0/255, blue:225.0/255, alpha:1.0)
         indicatorStyle = .white
         buildingContainerView.frame = CGRect(origin: .zero, size: contentSize)
     }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let superResult = super.hitTest(point, with: event)
+        if moonView!.frame.contains(point){
+            return moonView!
+        }
+        return superResult
+    }
+    
+    private func createMoon() -> UIView {
+        let side = 100
+        let size = CGSize(width: side, height: side)
+        let circle = UIView(frame: CGRect(origin: .zero, size: size))
+        circle.backgroundColor = .white
+        circle.layer.cornerRadius = CGFloat(side) * 0.5
+        
+        let moveGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
+        circle.addGestureRecognizer(moveGesture)
+        
+        return circle
+    }
+    
+    @objc func handleLongPress(_ gesture: UILongPressGestureRecognizer){
+        guard let movedCircle = gesture.view else { return }
+        
+        switch gesture.state {
+        case .began:
+            let grabPoint = gesture.location(in: self)
+            let circleCenter = movedCircle.center
+            let grabOffset = CGVector(dx: grabPoint.x - circleCenter.x, dy: grabPoint.y - circleCenter.y)
+            self.grabOffsetVector = grabOffset
+            self.sendSubviewToBack(movedCircle)
+            UIView.animate(withDuration: 0.2) {
+                movedCircle.alpha = 0.5
+                movedCircle.transform = .init(scaleX: 1.2, y: 1.2)
+            }
+        case .changed:
+            let grabOffset = self.grabOffsetVector
+            let touchLocation = gesture.location(in: self)
+            movedCircle.center = CGPoint(x: touchLocation.x - grabOffset.dx, y: touchLocation.y - grabOffset.dy)
+            originalOffset = abs(movedCircle.frame.origin.x - self.bounds.maxX)
+        case .ended, .cancelled:
+            UIView.animate(withDuration: 0.2) {
+                movedCircle.alpha = 1
+                movedCircle.transform = .identity
+            }
+        default:
+            break
+        }
+    }
+    
 }
